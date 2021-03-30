9cf5e53008af8039aeffa807386bc61a730efa53
Omar <omarashrf15@gmail.com>
2021-03-30 15:46:11 +0200

    added model association

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+    validates :channel_id, presence: true
+    validates :user_id, presence: true
+    validates :title, presence: true, {maximum: 127}
+    validates :body, presence: true
+    belongs_to :user
+    belongs_to :channel
 end
