c7df7fad7234ea2a44d7f1e278570f67679e4160
Omar Ashraf <omarashrf15@gmail.com>
2021-03-30 16:58:11 +0200

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
     validates :channel_id, presence: true
     validates :user_id, presence: true
-    validates :title, presence: true, {maximum: 127}
+    validates :title, presence: true, length: {maximum: 127}
     validates :body, presence: true
     belongs_to :user
     belongs_to :channel
