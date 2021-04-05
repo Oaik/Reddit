861ac7f2e1b8f3f3999e497c757ea52aa1cfa1d1
Omar <omarashrf15@gmail.com>
2021-04-05 12:29:02 +0200

    front-end update

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -3,7 +3,11 @@ class HomepageController < ApplicationController
   # GET /
   def index
     if user_signed_in?
-      redirect_to "/posts"
+      # redirect_to "/posts"
+      @posts = Post.all
+      @channels = Channel.all
+    else
+      redirect_to "/users/sign_in"
     end
   end
 end
